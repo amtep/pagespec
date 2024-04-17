3507e6d1bc9e638b88d9742896a9c989fcb6e20f
Richard Braakman <shuttlegit@gmail.com>
2024-04-17 10:15:21 +0300

    add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
 //! A module for parsing ikiwiki's `PageSpec` expressions into abstract syntax trees.
 
+/* Copyright (C) 2024  Richard Braakman
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use winnow::ascii::{digit1, space1};
 use winnow::combinator::{alt, cut_err, delimited, preceded, separated, trace};
 pub use winnow::error::{ContextError, ParseError};
